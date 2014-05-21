ed04b77e4709ab4f1eed83140d4c746bd1b749ca
kenilkdoshi@gmail.com <kenilkdoshi@gmail.com@9ca13b58-f4ea-88db-2bd4-fcb8172b7e70>
2014-05-21 19:35:10 +0000

    Adding Wall Post feature. Adding gallery page controller. /school/{id}/{page}/

--- trunk/src/main/java/com/linkalma/dao/IUserUpdateDAO.java
+++ trunk/src/main/java/com/linkalma/dao/IUserUpdateDAO.java
@@ -1,11 +1,13 @@
 package com.linkalma.dao;
 
 import java.util.List;
+
 import com.linkalma.dto.UserUpdateDTO;
+import com.linkalma.dto.WallPostDto;
 
 public interface IUserUpdateDAO {
 	
-	public int createWallPost(UserUpdateDTO userUpdateDto);
+	public long createWallPost(WallPostDto wallPostDto);
 	
 	public List<UserUpdateDTO> getUserWallPost(long userID);
 
--- trunk/src/main/java/com/linkalma/dao/impl/UserUpdateDAO.java
+++ trunk/src/main/java/com/linkalma/dao/impl/UserUpdateDAO.java
@@ -1,18 +1,22 @@
 package com.linkalma.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
-import java.util.Map;
 
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 
 import com.linkalma.dao.IUserUpdateDAO;
-import com.linkalma.dao.mapper.SchoolMapper;
 import com.linkalma.dao.mapper.UserUpdateMapper;
-import com.linkalma.dto.School;
 import com.linkalma.dto.UserUpdateDTO;
+import com.linkalma.dto.WallPostDto;
 import com.linkalma.utils.ApplicationConstants;
 
 public class UserUpdateDAO implements IUserUpdateDAO {
@@ -25,9 +29,27 @@ public class UserUpdateDAO implements IUserUpdateDAO {
 	
 
 	@Override
-	public int createWallPost(UserUpdateDTO userUpdateDto) {
-		// TODO Auto-generated method stub
-		return 0;
+	public long createWallPost(final WallPostDto wallPostDto) {
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+	      
+		getJdbcTemplateObject().update(new PreparedStatementCreator() {
+    	  	public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+	    	  	PreparedStatement ps =  connection.prepareStatement(ApplicationConstants.INSERT_WALLPOST_QUERY, 
+	    	  			new String[] {"id"});
+	    	  	
+	    	  	ps.setLong(1, wallPostDto.getUserID());
+	    	  	ps.setString(2, "");
+	    	  	ps.setString(3, wallPostDto.getWallPost());
+	    	  	ps.setInt(4, wallPostDto.getPostTo());
+	    	  	ps.setLong(5, wallPostDto.getUserID());
+	    	  	ps.setLong(6, wallPostDto.getUserID());
+	    	  	
+	    	  	return ps;
+    	  	}
+	  	}, keyHolder);
+      
+      long newPersonId = keyHolder.getKey().longValue();
+      return newPersonId;
 	}
 
 	@Override
