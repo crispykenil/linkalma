cae14a06c44fb62d7eb94c33b04ec2f82d1cbcac
kumbharnilesh.a <kumbharnilesh.a@gmail.com>
2014-06-15 18:50:46 +0000

    Handle resultset size 0 error.

--- src/main/java/com/linkalma/dao/impl/LoginDAO.java
+++ src/main/java/com/linkalma/dao/impl/LoginDAO.java
@@ -2,8 +2,12 @@ package com.linkalma.dao.impl;
 
 import javax.sql.DataSource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+
 
 import com.linkalma.dao.ILoginDAO;
 import com.linkalma.dao.mapper.SchoolLoginMapper;
@@ -11,43 +15,58 @@ import com.linkalma.dao.mapper.UserLoginMapper;
 import com.linkalma.dto.UserBean;
 import com.linkalma.utils.ApplicationConstants;
 
-public class LoginDAO implements ILoginDAO{
+public class LoginDAO implements ILoginDAO {
 
 	@Autowired
 	private DataSource dataSource;
-   
+
 	@Autowired
-   private JdbcTemplate jdbcTemplateObject;
-	
-	
+	private JdbcTemplate jdbcTemplateObject;
+
 	@Override
 	public void setDataSource(DataSource dataSource) {
 		this.dataSource = dataSource;
 	}
-	
-	
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(LoginDAO.class);
+
 	@Override
 	public UserBean validateUserCredentials(String userName, String password) {
-		
-		UserBean userBean = (UserBean) getJdbcTemplateObject().queryForObject(
-				ApplicationConstants.VALIDATE_USER_QUERY,
-				new Object[] { userName, password }, new UserLoginMapper());
 
-		return userBean;
-		
+		try {
+			UserBean userBean = (UserBean) getJdbcTemplateObject()
+					.queryForObject(ApplicationConstants.VALIDATE_USER_QUERY,
+							new Object[] { userName, password },
+							new UserLoginMapper());
+
+			return userBean;
+		} catch (EmptyResultDataAccessException e) {
+			logger.info("Error While accessing validateUserCredentials method",
+					e);
+		}
+
+		return null;
+
 	}
 
 	@Override
 	public UserBean validateSchoolCredentials(String userName, String password) {
-		
-		UserBean userBean = (UserBean) getJdbcTemplateObject().queryForObject(
-				ApplicationConstants.VALIDATE_SCHOOL_QUERY,
-				new Object[] { userName, password }, new SchoolLoginMapper());
+		try {
+			UserBean userBean = (UserBean) getJdbcTemplateObject()
+					.queryForObject(ApplicationConstants.VALIDATE_SCHOOL_QUERY,
+							new Object[] { userName, password },
+							new SchoolLoginMapper());
 
-		return userBean;
-		
+			return userBean;
+		} catch (EmptyResultDataAccessException e) {
+			logger.info("Error While accessing validateSchoolCredentials method",
+					e);
+		}
+
+		return null;
+
 	}
-
 
 	/**
 	 * @return the jdbcTemplateObject
@@ -56,14 +75,13 @@ public class LoginDAO implements ILoginDAO{
 		return jdbcTemplateObject;
 	}
 
-
 	/**
-	 * @param jdbcTemplateObject the jdbcTemplateObject to set
+	 * @param jdbcTemplateObject
+	 *            the jdbcTemplateObject to set
 	 */
 	public void setJdbcTemplateObject(JdbcTemplate jdbcTemplateObject) {
 		this.jdbcTemplateObject = jdbcTemplateObject;
 	}
-
 
 	/**
 	 * @return the dataSource
